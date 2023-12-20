f6505db8c783423f84e7d5241c86f9c7aef35d96
Prayash Shrestha <xrestha17@gmail.com>
2023-12-19 19:30:27 -0600

    project setup done

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/routes"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "public", "404Error.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server start on port ${PORT}`);
+});
